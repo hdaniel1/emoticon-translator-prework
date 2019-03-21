2975e08e9e447e2dd129c0a1482dcc4cf3b87ee9
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-21 14:04:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def load_library(file)
     translation_hash["get_emoticon"] = get_emoticon
     translation_hash["get_meaning"] = get_meaning
     translation_hash
-    #binding.pry
+    binding.pry
 end
 
 def get_japanese_emoticon
